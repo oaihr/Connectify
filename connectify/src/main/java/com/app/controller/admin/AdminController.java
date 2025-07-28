c871c1e515e7d68dc6c52aa7e9603de3670ccde0
oaihr <rhksgh0710@gmail.com>
2025-07-28 17:02:49 +0900

    프로젝트 초기 세팅, 테스트 & gitignore 설정

---
+++ connectify/src/main/java/com/app/controller/admin/AdminController.java
@@ -0,0 +1,25 @@
+package com.app.controller.admin;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class AdminController {
+	
+
+	@GetMapping("/main")
+	public String main() {
+		return "main";
+	}
+	
+}
